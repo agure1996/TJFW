6920b7768274af38a081dc37a2186b45865599e7
A <abbasgure@gmail.com>
2026-01-25 20:21:10 +0000

    Created barebones springapp as well as psotgresdb, placed template app properties

---
+++ src/main/java/com/example/tjfw/TjfwApplication.java
@@ -0,0 +1,13 @@
+package com.example.tjfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjfwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjfwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tjfw/TjfwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tjfw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjfwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
