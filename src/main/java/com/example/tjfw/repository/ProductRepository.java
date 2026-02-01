a0431501259b865f2b5ca150168bb4d9d058c317
A <abbasgure@gmail.com>
2026-02-01 17:38:13 +0000

    Added product controller and repository to test endpoints

--- src/main/java/com/example/tjfw/repository/ProductRepository.java
+++ src/main/java/com/example/tjfw/repository/ProductRepository.java
@@ -1,18 +1,8 @@
 package com.example.tjfw.repository;
 
 import com.example.tjfw.entity.Product;
-import com.example.tjfw.entity.ProductVariant;
-import com.example.tjfw.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    List<ProductVariant> findByProductId(Long productId);
-    List<Purchase> findBySupplierId(Long supplierId);
-    Optional<Product> findProductById(Long id);
-}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
--- src/main/java/com/example/tjfw/service/ProductService.java
+++ src/main/java/com/example/tjfw/service/ProductService.java
@@ -1,13 +1,22 @@
 package com.example.tjfw.service;
 
+import com.example.tjfw.entity.Product;
 import com.example.tjfw.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {this.productRepository = productRepository;}
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> findById(Long id){
+        return productRepository.findById(id);
+    }
 
 }
