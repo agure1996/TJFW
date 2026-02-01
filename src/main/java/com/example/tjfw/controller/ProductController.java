408cb355e8e9582622cfb45aa67e2b858bdb90c5
A <abbasgure@gmail.com>
2026-02-01 21:41:23 +0000

    added implementation for crud functions, product controller, as well as the business logic in service class

--- src/main/java/com/example/tjfw/controller/ProductController.java
+++ src/main/java/com/example/tjfw/controller/ProductController.java
@@ -1,34 +1,50 @@
 package com.example.tjfw.controller;
 
 import com.example.tjfw.entity.Product;
+import com.example.tjfw.exceptions.NotFoundException;
+import com.example.tjfw.response.ApiResponse;
 import com.example.tjfw.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/products")
 public class ProductController {
 
-
-    private final ProductService  productService;
+    private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     @GetMapping("")
-    public String sayHi(){
+    public String sayHi() {
         return "Hello World!";
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> findById(@RequestParam Long id){
-        return productService.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<ApiResponse<Product>> findById(@PathVariable Long id) {
+
+        Product product = productService.findById(id)
+                .orElseThrow(() -> new NotFoundException("Product not found with id " + id));
+
+        return ResponseEntity.ok(
+                new ApiResponse<>("Successfully found product with id " + id, product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Product>> createProduct(@Valid @RequestBody Product product) {
+            Product newProduct = productService.createNewProduct(product);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ApiResponse<>("Product created successfully", newProduct));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok(new ApiResponse<>("Product deleted successfully", null));
     }
 
 }
--- src/main/java/com/example/tjfw/service/ProductService.java
+++ src/main/java/com/example/tjfw/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.example.tjfw.service;
 
 import com.example.tjfw.entity.Product;
+import com.example.tjfw.exceptions.AlreadyExistsException;
+import com.example.tjfw.exceptions.NotFoundException;
 import com.example.tjfw.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +17,21 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public Optional<Product> findById(Long id){
+    public Product createNewProduct(Product product) {
+        if (productRepository.existsByProductName(product.getProductName())) {
+            throw new AlreadyExistsException("Product already exists");
+        }
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> findById(Long id) {
         return productRepository.findById(id);
+    }
+
+    public void deleteProduct(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Product with id: " + id + " not found"));
+        productRepository.delete(product);
     }
 
 }
